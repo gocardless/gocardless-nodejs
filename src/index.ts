9360f158491fda770147cf8568f5a45da2d61e10
Simon Grimshaw <simongrimshaw@gocardless.com>
2020-03-12 14:06:45 +0000

    Cleaning up files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { GoCardlessClient } from './GoCardlessClient';
-
-const initialiser = function(token, envrionment, options = {}) {
-  return new GoCardlessClient(token, envrionment, options);
-};
-
-module.exports = initialiser;
--- src/types/utils/json.ts
+++
@@ -1,6 +0,0 @@
-type JsonField = boolean | number | string | null;
-
-export interface JsonMap {
-  [key: string]: JsonField | JsonMap | JsonArray;
-}
-export interface JsonArray extends Array<JsonField> {}
