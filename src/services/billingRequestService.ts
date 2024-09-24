f093f5f655815d8a5d3efe76b7c71a443bba6aae
Robot <robot@gocardless.com>
2024-09-24 14:32:23 +0000

    Changes generated by ac5cfeab39308a479250729d34f91fd008e0a8ed

--- src/services/billingRequestService.ts
+++ src/services/billingRequestService.ts
@@ -44,6 +44,9 @@ interface BillingRequestCreateRequest {
   // and `other` are supported.
 
   purpose_code?: Types.BillingRequestPurposeCode;
+
+  //
+  subscription_request?: Types.BillingRequestSubscriptionRequest;
 }
 
 interface BillingRequestCollectCustomerDetailsRequest {
